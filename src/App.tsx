4f24eb525810101912433b8fae05fa544f2dca7e
PsmD <tbvjeld7@naver.com>
2022-01-16 22:14:07 +0900

    Creating Tasks

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,9 @@ const Wrapper = styled.div`
 `;
 
 const Boards = styled.div`
-display: flex;
-justify-content: center;
-align-items: flex-start;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
   width: 100%;
   gap: 10px;
 `;
@@ -29,8 +29,9 @@ function App() {
     if (destination?.droppableId === source.droppableId) {
       setToDos((allBoards) => {
         const boardCopy = [...allBoards[source.droppableId]];
+        const taskObj = boardCopy[source.index];
         boardCopy.splice(source.index, 1);
-        boardCopy.splice(destination?.index, 0, draggableId);
+        boardCopy.splice(destination?.index, 0, taskObj);
         return {
           ...allBoards,
           [source.droppableId]: boardCopy,
@@ -38,12 +39,12 @@ function App() {
       });
     }
     if (destination.droppableId !== source.droppableId) {
-      // cross board movement
       setToDos((allBoards) => {
         const sourceBoard = [...allBoards[source.droppableId]];
+        const taskObj = sourceBoard[source.index];
         const destinationBoard = [...allBoards[destination.droppableId]];
         sourceBoard.splice(source.index, 1);
-        destinationBoard.splice(destination?.index, 0, draggableId);
+        destinationBoard.splice(destination?.index, 0, taskObj);
         return {
           ...allBoards,
           [source.droppableId]: sourceBoard,
